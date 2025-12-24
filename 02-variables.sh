be3a77f7b7f89a8fa46759ac436bb7205a3db50d
kalyan <kalyankalaven@gmail.com>
2025-12-24 16:13:03 +0530

    conversation added

---
+++ 02-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "kalyan:: hi chinna"
+echo " chinna :: hi kalyan"
+echo " kalyan:: how are you?"
+echo " chinna :: i am fine"
+echo " kalyan:: what about you?"
+echo " chinna :: me too"
+echo " kalyan:: great to hear that"
+echo " chinna :: yes"
+echo " kalyan:: see you later"
+echo " chinna :: bye"
