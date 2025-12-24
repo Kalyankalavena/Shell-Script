c3ad199edb71f830b10975853e368736e72bf006
kalyan <kalyankalaven@gmail.com>
2025-12-24 16:54:38 +0530

    conversation added

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,6 +2,6 @@
 
 echo "please enter your user name::" # text entered here will be added as value to variable 
 
-read USERNAME  # Here username is a variable name 
+read -s USERNAME  # Here username is a variable name 
 
 echo "user name entered is :: $USERNAME"
