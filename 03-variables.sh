ca4f4bb3a835860f86fdbf716b6610b685e5455f
kalyan <kalyankalaven@gmail.com>
2025-12-24 16:22:22 +0530

    conversation added

---
+++ 03-variables.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PERSON1="kalyan" # No spaces before and after equal sign
+PERSON2="chinna"
+
+echo "$PERSON1:: hi $PERSON2"
+echo " $PERSON2 :: hi $PERSON1"
+echo "$PERSON1:: hi $PERSON2"
+echo " $PERSON2 :: hi $PERSON1"
+echo "$PERSON1:: how are you?"
+echo " $PERSON2 :: i am fine"
+echo "$PERSON1:: what about you?"
+echo " $PERSON2 :: me too"
+echo "$PERSON1:: great to hear that"
+echo " $PERSON2 :: yes"
+echo "$PERSON1:: see you later"
+echo " $PERSON2 :: bye"
