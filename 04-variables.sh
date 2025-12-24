0a9e94b911bc1eb066acacafddeb6331e95c012e
kalyan <kalyankalaven@gmail.com>
2025-12-24 16:32:32 +0530

    conversation added

---
+++ 04-variables.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PERSON1= $1
+PERSON2= $2
+
+echo "$PERSON1:: hi $PERSON2"
+echo " $PERSON2 :: hi $PERSON1"
+echo "$PERSON1:: hi $PERSON2"
+echo " $PERSON2 :: hi $PERSON1"
+echo "$PERSON1:: how are you?"
+echo " $PERSON2 :: i am fine"
+echo "$PERSON1:: what about you?"
+echo " $PERSON2 :: me too"
+echo "$PERSON1:: great to hear that"
+echo " $PERSON2 :: yes"
+echo "$PERSON1:: see you later"
+echo " $PERSON2 :: bye"
