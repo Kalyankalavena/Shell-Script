aebb5dee9fe2256d3e722f8cba0989b8439f2c0f
kalyan <kalyankalaven@gmail.com>
2026-02-07 08:48:47 +0530

    s expense shell

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -2,11 +2,17 @@
 
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=5 #real projects will monitor for 70 
-
+MSG=""
 while read -r line
 do
     USAGE=$(echo $line | awk -F " " '{print $6F}' |cut -d "%" -f1)
     PARTITION=$(echo $line | awk -F " " '{print $NF}')
-    echo "partition: $PARTITION, usage: $USAGE"
-
+    #echo "partition: $PARTITION, usage: $USAGE"
+    if [ "$USAGE" -gt "$DISK_THRESHOLD" ]
+    then
+        MSG="High disk usage on partition $PARTITION usage is $USAGE"
+    fi
 done <<< "$DISK_USAGE"
+
+echo "MESSAGE: $MSG"
+
