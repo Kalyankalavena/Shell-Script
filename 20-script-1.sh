9e35bd5d3388d0c48a96afc9b8e59a2ebbab0be3
kalyan <kalyankalaven@gmail.com>
2026-02-07 15:40:43 +0530

    s expense shell

--- 20-script-1.sh
+++ 20-script-1.sh
@@ -2,7 +2,7 @@
 
 MSG="SCRIPT1"
 GREET="HI from Script1"
-SOURCE ./21-script-2.sh # this will execute the script and export the variables to current shell environment
+source ./21-script-2.sh # this will execute the script and export the variables to current shell environment
 
 echo "Hello from : $MSG"
 
