484df4dd5f413d3b2a4247e80f49ff6d38047c3d
kalyan <kalyankalaven@gmail.com>
2026-07-11 21:00:12 +0530

    conversation added

---
+++ Practice shell script/03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1="Mahesh" # no space before and after the equal sign
+person2="suresh"
+
+echo "$person1:: Hi $person2"
+echo "$person2:: Hello $person1"
+echo "$person1:: How are you doing?"
+echo "$person2:: I am doing good, how about you?"
