c5759e9323460a386819ddd6d28a7d2fb68b1aed
kalyan <kalyankalaven@gmail.com>
2026-07-13 20:09:13 +0530

    shell script

--- Practice shell script/10-install-script.sh
+++ Practice shell script/10-install-script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+USERID=$(id -u) 
+
+if [ $USERID -ne 0 ]; then
+   echo "Error :: This script must be run as root. Please run with sudo or as root user."
+fi
+
 dnf install mysql -y
-
-
-
-
