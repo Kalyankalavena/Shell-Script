bfb5db8428599fee11911b64c1632a60e6316488
kalyan <kalyankalaven@gmail.com>
2026-07-13 20:22:34 +0530

    shell script

--- Practice shell script/10-install-script.sh
+++ Practice shell script/10-install-script.sh
@@ -8,7 +8,26 @@ if [ $USERID -ne 0 ]; then
 fi
 
 dnf install mysql -y
+if [ $? -ne 0 ]; then
+   echo "installation of mysql failed. Please check the logs."
+   exit 1
+fi
+else
+    echo "installing mysql is successful."
+
 
 dnf install git -y
+if [ $? -ne 0 ]; then
+   echo "installation of git failed. Please check the logs."
+   exit 1   
+fi
+else
+    echo "installing git is successful."
 
 dnf install httpd -y
+if [ $? -ne 0 ]; then
+   echo "installation of httpd failed. Please check the logs."
+   exit 1   
+fi
+else
+    echo "installing httpd is successful."
