f5a1ba47df84367014ef7bdbd7c7914883eab765
kalyan <kalyankalaven@gmail.com>
2026-07-14 15:16:48 +0530

    shell script

--- Practice shell script/10-install-script.sh
+++ Practice shell script/10-install-script.sh
@@ -7,27 +7,51 @@ if [ $USERID -ne 0 ]; then
    exit 1 # other than 0 exit status indicates error
 fi
 
-dnf install mysql -y
-if [ $? -ne 0 ]; then
-   echo "installation of mysql failed "
-   exit 1
+dnf list installed mysql  
+
+if [ $? -ne 0]
+then # not installed
+    dnf install mysql -y
+    if [ $? -ne 0 ]; 
+    then
+      echo "installation of mysql failed "
+      exit 1
+    else
+      echo "installing mysql is successful."
+    fi
 else
-    echo "installing mysql is successful."
+    echo "mysql is already installed."
 fi
 
+dnf list installed git
 
-dnf install git -y
-if [ $? -ne 0 ]; then
-   echo "installation of git failed"
-   exit 1   
+if [ $? -ne 0 ]
+then # not installed
+    dnf install git -y
+    if [ $? -ne 0 ]; 
+    then
+      echo "installation of git failed "
+      exit 1
+    else
+      echo "installing git is successful."
+    fi
 else
-    echo "installing git is successful."
+    echo "git is already installed."
 fi
+ 
+# dnf install git -y
+# if [ $? -ne 0 ];
+# then
+#    echo "installation of git failed"
+#    exit 1   
+# else
+#     echo "installing git is successful."
+# fi
 
-dnf install httpd -y
-if [ $? -ne 0 ]; then
-   echo "installation of httpd failed."
-   exit 1   
-else
-    echo "installing httpd is successful."
-fi
+# dnf install httpd -y
+# if [ $? -ne 0 ]; then
+#    echo "installation of httpd failed."
+#    exit 1   
+# else
+#     echo "installing httpd is successful."
+# fi
